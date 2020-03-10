6dec8a22a8f26f3c0e0d6890afbf8f3850e73f06
B13RN0 <60328016+B13RN0@users.noreply.github.com>
2020-03-10 22:33:26 +0100

    Update Traffic Lights.c

--- Traffic Lights.c
+++ Traffic Lights.c
@@ -6,8 +6,14 @@
 
   int greenP = 6;
   int redP = 7;
+
+  bool glow;
+  int switchState;
+  int oldSwitchState;
   
 void setup() {
+
+    glow = false;
     
     pinMode(green, OUTPUT);
     pinMode(yellow, OUTPUT);
@@ -18,32 +24,27 @@ void setup() {
     digitalWrite(green, LOW);
     digitalWrite(yellow, LOW);
     digitalWrite(red, LOW);
-}
+} // end of 'void setup'
 
 void loop()
 {
-  while(digitalRead(button) == HIGH)
-  {
-    lightS();  
-  }
+  switchState = bitRead(PIND, 2);
+  if(oldSwitchState != switchState){
+    oldSwitchState = switchState;
+    delay(10);
+    if(switchState == 0){
+      changeLights();
+    }
+  }else {lightS();}
 
   delay(1000);
-  while(digitalRead(button) == LOW)
-  {
-        digitalWrite(green, LOW);
-        digitalWrite(yellow, LOW);
-        digitalWrite(red, HIGH);
-        delay(1000);
-        digitalWrite(redP, LOW);
-        digitalWrite(greenP, HIGH);
-        delay(10000);
-        button = HIGH;
-  }
+ 
   
-}
+} // end of 'void loop'
 
 void lightS()
   {
+    //      [R][g]  ||  [R][Y][g]        
       digitalWrite(redP, HIGH);
       digitalWrite(greenP, LOW);
       
@@ -51,23 +52,48 @@ void lightS()
       digitalWrite(yellow, HIGH);
       digitalWrite(red, HIGH);
 
-      delay(1000);
-      
+      delay(2500);
+
+    //      [R][g]  ||  [r][y][G] 
       digitalWrite(green, HIGH);
       digitalWrite(yellow, LOW);
       digitalWrite(red, LOW);
       
-      delay(1000);
+      delay(8000);
 
+    //      [R][g]  ||  [r][Y][g] 
       digitalWrite(green, LOW);
       digitalWrite(yellow, HIGH);
       digitalWrite(red, LOW);
       
-      delay(1000);
-  
+      delay(2500);
+
+    //      [R][g]  ||  [R][y][g] 
       digitalWrite(green, LOW);
       digitalWrite(yellow, LOW);
       digitalWrite(red, HIGH);
 
-      delay(1000);
+      delay(8000);
+  } // end of 'void lisghtS'
+
+void changeLights()
+  {
+      digitalWrite(redP, LOW);
+      digitalWrite(greenP, HIGH);
+      
+      digitalWrite(green, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(red, HIGH);
+
+      delay(3000);
+      for(int i=0; i<=5; i++){
+              digitalWrite(redP, LOW);
+              digitalWrite(greenP, HIGH);
+              delay(500);
+              digitalWrite(redP, LOW);
+              digitalWrite(greenP, LOW);
+              delay(500);
+              digitalWrite(redP, HIGH);
+              digitalWrite(greenP, LOW);
+         }
   }
